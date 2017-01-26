abcce418009288f4207ba5e234c54055255e6129
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-26 14:53:53 -0800

    cleaning up some paths, adding good seeds and flubbing around with robot index

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -1,19 +1,21 @@
 class Robot < ApplicationRecord
-  
+
   validates_presence_of :height, :weight, :model_no
   validates_numericality_of :height, :weight
 
-  private
 
-  def gen_model_no
-  manu = ['UR', 'MM', 'CS', 'BA', 'HM']
-  numbs = (0..9).to_a
-  suffix = ''
-    6.times do
-      suffix += numbs.sample.to_s
-    end
-  model_no = (manu.sample + suffix)
-  end
-  
+### method moved to seed file, should this be deleted?
+  # private
+
+  # def gen_model_no
+  #   manu = ['UR', 'MM', 'CS', 'BA', 'HM']
+  #   numbs = (0..9).to_a
+  #   suffix = ''
+  #     6.times do
+  #       suffix += numbs.sample.to_s
+  #     end
+  #   model_no = (manu.sample + suffix)
+  # end
+
 end
 
