29d268e7fef166aacb4a3fe0a9ea8799b3af2c37
Deel With It <email": null>
2017-01-26 15:22:45 -0800

    rebases with master branch

--- app/models/robot.rb
+++ app/models/robot.rb
@@ -3,17 +3,5 @@ class Robot < ApplicationRecord
   validates_presence_of :height, :weight, :model_no
   validates_numericality_of :height, :weight
 
-  private
-
-  def gen_model_no
-  manu = ['UR', 'MM', 'CS', 'BA', 'HM']
-  numbs = (0..9).to_a
-  suffix = ''
-    6.times do
-      suffix += numbs.sample.to_s
-    end
-  model_no = (manu.sample + suffix)
-  end
-  
 end
 
