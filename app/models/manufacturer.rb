5e10a9985e7a6f4ea639ad4231ee1d0ada40f515
groovestation31785 <email": null>
2017-01-27 15:40:56 -0800

    Update manu tests

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,21 +1,18 @@
 class Manufacturer < ActiveRecord::Base
   has_many :prototypes
 
-  
   def self.names
     manufacturers_names
   end
 
-  validates :name, presence: true#,
-    # inclusion: { in: name, message: "%{value} is not someone with whom we do business!" }
-
+  validates :name, presence: true
 
 private
 
   def manufacturers_names
     manufacturers = HTTParty.get("http://jordankamin.com/robots_api/robots.json")
-    name_array = []   
-    manufacturers.parsed_response["manufacturers"].each do |man|  
+    name_array = []
+    manufacturers.parsed_response["manufacturers"].each do |man|
       name_array << man["name"]
     end
     name_array
@@ -23,4 +20,3 @@ private
 
 
 end
-
--- spec/models/manufacturer_spec.rb
+++ spec/models/manufacturer_spec.rb
@@ -4,17 +4,13 @@ require 'rails_helper'
   let(:manufacturer) { create(:manufacturer) }
   describe 'attributes' do
     it 'has a name' do
-      expect(manufacturer.name).to eq "The Confederation of Ham Manufacturing Concern"
+      expect(manufacturer.name).to eq "Stamm Inc"
     end
   end
 
     describe 'validations' do
-      it 'is valid when its name comes from the special list of manufacturers' do
-        should validate_inclusion_of(:name).in_array(["US Robotics", "Mechanical Men Group", "Cyberdyne Systems", "Barry's Automotons", "The Confederation of Ham Manufacturing Concern"])
-      end
-
-      it 'is not valid when its name comes from the special list of manufacturers' do
-        should_not allow_values('Sirius Cybernetics Corps').for(:name)
+      it 'is valid when its name is present' do
+        should validate_presence_of(:name)
       end
     end
 
