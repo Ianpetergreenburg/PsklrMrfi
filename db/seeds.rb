e34c9d8b7f3e927eca3731c50251c629c165f213
groovestation31785 <email": null>
2017-01-26 16:16:06 -0800

    Update seed file and add css styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,19 @@ Manufacturer.create(name: "Cyberdyne Systems")
 Manufacturer.create(name: "Barry's Automotons")
 Manufacturer.create(name: "The Confederation of Ham Manufacturing Concern")
 
+def gen_model_no
+  manu = ['UR', 'MM', 'CS', 'BA', 'HM']
+  numbs = (0..9).to_a
+  suffix = ''
+    6.times do
+      suffix += numbs.sample.to_s
+    end
+  model_no = (manu.sample + suffix)
+end
 
 def gen_proto_model_no(manufacturer)
   abbreviations = ['UR', 'MM', 'CS', 'BA', 'HM']
-  manufacturers = ["US Robotics", "Mechanical Men Group", "Cyberdyne Systems", "Barry's Automotons", "The Confederate of Ham Manufacturing Concern"]
+  manufacturers = Manufacturer.names
   index = manufacturers.index(manufacturer)
   initials = abbreviations[index]
   numbs = (0..9).to_a
