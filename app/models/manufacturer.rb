e756da353ffade931150c7c0aab973ef75c0072c
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-27 14:24:15 -0800

    fixing for deploy

--- app/models/manufacturer.rb
+++ app/models/manufacturer.rb
@@ -1,10 +1,12 @@
 class Manufacturer < ActiveRecord::Base
   has_many :prototypes
 
-  
+
   def self.names
-    manufacturers_names
+    ["US Robotics", "Mechanical Men Group", "Cyberdyne Systems", "Barry's Automotons", "The Confederate of Ham Manufacturing Concern"]
   end
+
+
 
   validates :name, presence: true#,
     # inclusion: { in: name, message: "%{value} is not someone with whom we do business!" }
@@ -14,8 +16,8 @@ private
 
   def manufacturers_names
     manufacturers = HTTParty.get("http://jordankamin.com/robots_api/robots.json")
-    name_array = []   
-    manufacturers.parsed_response["manufacturers"].each do |man|  
+    name_array = []
+    manufacturers.parsed_response["manufacturers"].each do |man|
       name_array << man["name"]
     end
     name_array
