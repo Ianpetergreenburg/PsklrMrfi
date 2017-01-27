9077dc0a1681a5a6857f8cba009c8b11105a07ef
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-27 14:55:12 -0800

    fixing js

--- app/assets/javascripts/prototype.js
+++ app/assets/javascripts/prototype.js
@@ -2,9 +2,8 @@ $(document).ready(function() {
   $('body').on("click", ".prototype-link", function(event){
     event.preventDefault()
     $('.prototype-details').remove()
-    var id = $(this).prop('id').replace(/prototype-info-link-/, '')
+    var id = $(this).attr('id')
     var $this = $(this).parent()
-    console.log($(this))
     $.ajax({
       url: '/prototypes/' + id,
       type: 'get',
