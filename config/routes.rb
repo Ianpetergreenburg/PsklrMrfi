c46b2d47c1f1aaa7c0a98f8ac6e5c2bd022bbedb
groovestation31785 <email": null>
2017-01-28 14:13:49 -0800

    Create index_customer route

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :robots
+  get '/index_customer' => 'robots#index_customer'
   resources :prototypes, only: [:show]
   resources :users
   resources :manufacturers, only: [:show]
