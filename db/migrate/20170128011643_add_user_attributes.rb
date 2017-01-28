7ee673626c371ebc98d2e32fe14b0ffeee557689
Ian Peter Greenburg <iangreenburg@yahoo.com>
2017-01-27 19:23:24 -0800

    Update 20170128011643_add_user_attributes.rb

--- db/migrate/20170128011643_add_user_attributes.rb
+++ db/migrate/20170128011643_add_user_attributes.rb
@@ -3,6 +3,6 @@ class AddAttributesToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :email, :string
-    add_column :users, :card_number, :string
+    add_column :users, :card_number, :integer
   end
 end
