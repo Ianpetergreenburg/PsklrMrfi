4e9c4bc491562bdbd1da81ad5c15a772d965f8b0
groovestation31785 <email": null>
2017-01-27 16:53:24 -0800

    Add more routes for user

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   delete '/logout',  to: 'sessions#destroy'
   resources :robots
   resources :prototypes, only: [:show]
-  resources :users, only: [:show]
+  resources :users
   resources :manufacturers, only: [:show]
   get '/tina' => 'entrances#tina'
   get '/login_tina' => 'entrances#login_tina'
