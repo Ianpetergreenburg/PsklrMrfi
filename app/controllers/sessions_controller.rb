1c487da9f1ef7b5403924fa539279549868efcab
groovestation31785 <email": null>
2017-01-27 17:04:30 -0800

    Adjust controllers for admin permission

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
   include SessionsHelper
+
   def new
     @user = User.new
     render "new"
   end
 
   def create
-    user = User.find_by(username: params[:session][:username].downcase)
-    if user && user.authenticate(params[:session][:password])
+    user = User.find_by(username: params_username)
+    if user && user.authenticate(params_password)
       log_in(user)
-      redirect_to  robots_path #different path!
+      if current_user_commissioner
+        redirect_to robots_path
+      else
+        # redirect_to user home page
+      end
     else
       flash[:notice] = "You must be logged in with proper credentials"
       render 'new'
@@ -22,6 +27,14 @@ class SessionsController < ApplicationController
   end
 
   private
+  def params_username
+    params[:session][:username]
+  end
+
+  def params_password
+    params[:session][:password]
+  end
+
   def session_params
     params.require(:session).permit(:username, :password)
   end
