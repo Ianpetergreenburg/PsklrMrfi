cbc926b92f9e299346073c94d364582246a70fc2
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-28 22:57:58 -0800

    adding in helper test:

--- spec/helpers/prototype_helper_spec.rb
+++ spec/helpers/prototype_helper_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 describe PrototypeHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'get_attributes_for_robot' do
+    it 'retrieves the attributes of a prototype' do
+      prototype = create(:prototype)
+
+      attributes = attributes_for(:prototype)
+      attributes = attributes.inject({}){|memo,(k,v)| memo[k.to_s] = v; memo}
+      attributes['price'] = attributes['price'] * 2
+      attributes['pending'] = true
+      attributes['manufacturer_id'] = prototype.manufacturer_id
+
+      expect(get_attributes_for_robot(prototype)).to eq attributes
+    end
+  end
 end
