06537b143a7060e7127d81cfcb698cc5d2621f3f
Deel With It <email": null>
2017-01-29 11:59:15 -0800

    merging with master

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,6 @@
 class PrototypesController < ApplicationController
   def show
+    if logged_in?
     @prototype = Prototype.find(params[:id])
     @manufacturer = Manufacturer.find(@prototype.manufacturer_id)
     respond_to do |format|
