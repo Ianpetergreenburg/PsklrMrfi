0f6b313e4b6676578f158d8a7675f401ebd80427
Deel With It <email": null>
2017-01-29 13:52:27 -0800

    plugs 'security' holes that allow users to circumvent login

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,11 +1,16 @@
 class PrototypesController < ApplicationController
+  
   def show
-    if logged_in?
+    if current_user_commissioner
     @prototype = Prototype.find(params[:id])
+    p @prototype
     @manufacturer = Manufacturer.find(@prototype.manufacturer_id)
     respond_to do |format|
-      format.html { render '_show', layout: false}
+      format.html { render 'show', layout: false}
       format.js
+    end
+  else
+    redirect_to index_customer_path
     end
   end
 end
