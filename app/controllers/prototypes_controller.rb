bc6866c2b56db46eafc3220230872799c653e2e5
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-29 15:18:37 -0800

    small changes

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,9 +1,8 @@
 class PrototypesController < ApplicationController
-  
+
   def show
     if current_user_commissioner
     @prototype = Prototype.find(params[:id])
-    p @prototype
     @manufacturer = Manufacturer.find(@prototype.manufacturer_id)
     respond_to do |format|
       format.html { render 'show', layout: false}
