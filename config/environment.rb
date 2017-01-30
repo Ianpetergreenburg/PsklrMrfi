15827f6668bdbee740fa62d3db2a079b5d1a28dd
groovestation31785 <email": null>
2017-01-29 16:05:41 -0800

    Config to send emails through Heroku

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ ActionMailer::Base.smtp_settings =
     :port               => 587,
     :domain             => 'heroku.com',
     :authentication     => :plain,
-    :user_name          => ENV['SENDGRID_USERNAME'],
-    :password           => ENV['SENDGRID_PASSWORD'],
+    :user_name          => ENV["SENDGRID_USERNAME"],
+    :password           => ENV["SENDGRID_PASSWORD"],
     :enable_startstls_auto => true
   }
