a41abc38402c4eb2d062da15feaf252befed11e5
Ian Peter Greenburg & Kim Tran <pair+ianpetergreenburg.kimtran27@devbootcamp.com>
2017-01-29 18:03:26 -0800

    starting reg and login features specs

---
+++ spec/features/login_page_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'visiting the login page' do
+  let!(:user) { create(:user) }
+
+  scenario 'there is a login form' do
+    visit '/login'
+    expect(page).to have_css('div.login_form')
+  end
+
+  scenario 'filling out form with legit info logs you in' do
+    visit '/login'
+    fill_in('Username', :with => user.username)
+    fill_in('Password', :with => attributes_for(:user)[:password])
+    expect(page).to have_css('div.login_form')
+  end
+end
